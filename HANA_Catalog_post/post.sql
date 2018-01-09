4774050b0eedf710d585d37fab14aee2282cf159
Onno Bagijn <22906446+Balonno@users.noreply.github.com>
2018-01-09 14:04:05 +0100

    Update post hana statements

--- HANA_Catalog_post/post.sql
+++ HANA_Catalog_post/post.sql
@@ -1,1 +1,2 @@
-grant select on schema SYSTEM to _SYS_REPO with grant option;
+grant select, execute on schema SYSTEM to _SYS_REPO with grant option;
+grant select, execute on schema 3_HANA_HADOOP_IMAGEPROCESSING to _SYS_REPO with grant option;
