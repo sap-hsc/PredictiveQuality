21ae1b7bb07a4559f717ce6f940e32fd0cfc404d
Balonno <22906446+Balonno@users.noreply.github.com>
2018-01-10 12:12:22 +0100

    Update SYS_REPO auth.

--- HANA_Catalog_post/post.sql
+++ HANA_Catalog_post/post.sql
@@ -1,2 +1,2 @@
-grant select, execute on schema SYSTEM to _SYS_REPO with grant option;
-grant select, execute on schema 3_HANA_HADOOP_IMAGEPROCESSING to _SYS_REPO with grant option;
+grant select, execute, update, insert, delete on schema SYSTEM to _SYS_REPO with grant option;
+grant select, execute, update, insert, delete on schema 3_HANA_HADOOP_IMAGEPROCESSING to _SYS_REPO with grant option;
