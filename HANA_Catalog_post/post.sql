9a04831ede2fe05327590f2898728be2c700a7d4
Balonno <22906446+Balonno@users.noreply.github.com>
2018-01-11 19:54:17 +0100

    Update grant HANA Schema

--- HANA_Catalog_post/post.sql
+++ HANA_Catalog_post/post.sql
@@ -1,2 +1,4 @@
 grant select, execute, update, insert, delete on schema SYSTEM to _SYS_REPO with grant option;
 grant select, execute, update, insert, delete on schema 3_HANA_HADOOP_IMAGEPROCESSING to _SYS_REPO with grant option;
+grant select, execute, update, insert, delete on schema 3_HANA_HADOOP_IMAGEPROCESSING to DATAHUB_1 with grant option;
+grant select on schema SYSTEM to DATAHUB_1 with grant option;
