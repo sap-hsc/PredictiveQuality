be31c3b01fad6cf032c3817ff1104f31a787ebbe
Balonno <22906446+Balonno@users.noreply.github.com>
2018-04-04 21:24:59 +0200

    UI updates and reset demo default

--- HANA_Catalog_post/post.sql
+++ HANA_Catalog_post/post.sql
@@ -1,4 +1,5 @@
 grant select, execute, update, insert, delete on schema SYSTEM to _SYS_REPO with grant option;
 grant select, execute, update, insert, delete on schema 3_HANA_HADOOP_IMAGEPROCESSING to _SYS_REPO with grant option;
 grant select, execute, update, insert, delete on schema 3_HANA_HADOOP_IMAGEPROCESSING to DATAHUB_1 with grant option;
+CALL "3_HANA_HADOOP_IMAGEPROCESSING"."3_HANA_HADOOP_IMAGEPROCESSING::TRUNCATE_TABLES";
 grant select on schema SYSTEM to DATAHUB_1 with grant option;
