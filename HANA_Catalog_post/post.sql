44bed70f52e085024ce1215a240abc6d2bf12f88
Balonno <22906446+Balonno@users.noreply.github.com>
2019-06-13 20:18:44 +0200

    Update post.sql
    update for DATAHUB_1 user

--- HANA_Catalog_post/post.sql
+++ HANA_Catalog_post/post.sql
@@ -2,4 +2,13 @@ grant select, execute, update, insert, delete on schema SYSTEM to _SYS_REPO with
 grant select, execute, update, insert, delete on schema 3_HANA_HADOOP_IMAGEPROCESSING to _SYS_REPO with grant option;
 grant select, execute, update, insert, delete on schema 3_HANA_HADOOP_IMAGEPROCESSING to DATAHUB_1 with grant option;
 CALL "3_HANA_HADOOP_IMAGEPROCESSING"."3_HANA_HADOOP_IMAGEPROCESSING::TRUNCATE_TABLES";
-grant select on schema SYSTEM to DATAHUB_1 with grant option;
+grant select, execute on schema SYSTEM to DATAHUB_1 with grant option;
+GRANT "AFL__SYS_AFL_AFLPAL_EXECUTE" TO "DATAHUB_1";
+GRANT "AFL__SYS_AFL_AFLPAL_EXECUTE_WITH_GRANT_OPTION" TO "DATAHUB_1";
+GRANT "AFL__SYS_AFL_APL_AREA_EXECUTE" TO "DATAHUB_1";
+GRANT "sap.pa.apl.base.roles::APL_EXECUTE" TO "DATAHUB_1";
+GRANT "AFL__SYS_AFL_APL_AREA_WITH_GRANT_OPTION" TO "DATAHUB_1";
+GRANT "AFL__SYS_AFL_AFLPAL_EXECUTE" TO "SYSTEM";
+GRANT "AFL__SYS_AFL_AFLPAL_EXECUTE_WITH_GRANT_OPTION" TO "SYSTEM";
+GRANT "AFL__SYS_AFL_APL_AREA_EXECUTE" TO "SYSTEM";
+GRANT "AFL__SYS_AFL_APL_AREA_WITH_GRANT_OPTION" TO "SYSTEM";
